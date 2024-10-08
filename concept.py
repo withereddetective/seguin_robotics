5f9721ba69ab3f6a093b15332b431e55438e5051
withereddetective <fnafgameplay44@gmail.com>
2024-10-08 15:18:02 -0500

    Update concept.py

--- concept.py
+++ concept.py
@@ -1,5 +1,15 @@
+import keyboard
+
+def motor_f():
+  print("motor going forward")
+
 def main():
   while True:
+    print(keyboard.read_key())
+    if keyboard.read_key() == 'w':
+      motor_f()
+    if keyboard.read_key() == 'p':
+      break
 
 if __name__ == '__main__':
   main()
