334ea16ddd176ec6848017f215ddb2f12159810b
Proton <electronicballs69@gmail.com>
2024-10-10 11:03:53 -0500

    Update concept.py
    better concep !!

--- concept.py
+++ concept.py
@@ -3,13 +3,26 @@ import keyboard
 def motor_f():
   print("motor going forward")
 
+def motor_b():
+  print("motor going backward")
+
+def motor_l():
+  print("wheels turning left")
+
+def motor_r():
+  print("wheels turning right")
+
 def main():
   while True:
     print(keyboard.read_key())
     if keyboard.read_key() == 'w':
-      motor_f()
-    if keyboard.read_key() == 'p':
-      break
+       motor_f()
+    if keyboard.read_key() == 's':
+      motor_b()
+    if keyboard.read_key() == 's':
+      motor_l()
+    if keyboard.read_key() == 's':
+      motor_r()
 
 if __name__ == '__main__':
   main()
